cb3e2fc1ea6abe256dc85fc9504c6969d88ac2a9
abidurrahman11 <abidurr247@gmail.com>
2022-10-03 14:34:57 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,69 @@
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+const count = 30;
+const apiKey = config.apiKey;
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+let photos = [];
+
+function imageLoaded() {
+    imagesLoaded++;
+    if (imagesLoaded === totalImages) {
+        ready = true;
+        loader.hidden = true;
+    }
+}
+
+function setAttributes(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
+function displayPhotos() {
+    imagesLoaded = 0;
+    totalImages = photos.length;
+
+    photos.forEach((photo) => {
+        const item = document.createElement('a');
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank',
+        });
+
+        const img = document.createElement('img');
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        });
+
+        img.addEventListener('load', imageLoaded);
+
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+    }); 
+}
+
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        photos = await response.json();
+        displayPhotos();
+    } catch (error) {
+        //err
+    }
+}
+
+window.addEventListener('scroll', () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+        ready = false;
+        getPhotos();
+    }
+});
+
+getPhotos();
